528400ce10b102283bce98f1957165b1746d096b
russellf9 <russell@factornine.co.uk>
2021-02-18 15:04:47 +0000

    :sparkles: Scrabble - test Hooks
    Add example with Reset

--- src/components/toggle/toggle.js
+++ src/components/toggle/toggle.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Switch from "./switch";
+
 function useToggle() {
   const [on, setOnState] = useState(false);
 
@@ -11,13 +12,24 @@ function useToggle() {
 }
 
 const Toggle = () => {
+  const [clicksSinceReset, setClicksSinceReset] = useState(0);
+  const tooManyClicks = clicksSinceReset >= 4;
+
   const { on, toggle, setOn, setOff } = useToggle();
+
+  function handleClick() {
+    toggle();
+    setClicksSinceReset((count) => count + 1);
+  }
 
   return (
     <div>
       <button onClick={setOff}>Switch Off</button>
       <button onClick={setOn}>Switch On</button>
-      <Switch on={on} onClick={toggle} />
+      <Switch on={on} onClick={handleClick} />
+      {tooManyClicks ? (
+        <button onClick={() => setClicksSinceReset(0)}>Reset</button>
+      ) : null}
     </div>
   );
 };
